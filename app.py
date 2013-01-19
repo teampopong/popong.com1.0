18ba3b554c2af6927ec64be6190637a197b400c5
Cheol Kang <steel@popong.com>
2013-01-20 02:15:49 +0900

    nginx reverse proxy에 물리기 위한 설정

--- app.py
+++ app.py
@@ -5,28 +5,24 @@ from flask import Flask, render_template, redirect
 
 import settings
 
-def init_routes(app):
+app = Flask(__name__)
 
-    @app.route('/')
-    def home():
-        return render_template('home.html', menus=settings.MENUS,
-                dirlinks=settings.DIRLINKS, active_page='Home')
+@app.route('/')
+def home():
+    return render_template('home.html', menus=settings.MENUS,
+            dirlinks=settings.DIRLINKS, active_page='Home')
 
-    @app.route('/blog')
-    def blog():
-        return redirect('http://blog.popong.com')
+@app.route('/blog')
+def blog():
+    return redirect('http://blog.popong.com')
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html', menus=settings.MENUS,
-                dirlinks=settings.DIRLINKS, active_page='About')
+@app.route('/about')
+def about():
+    return render_template('about.html', menus=settings.MENUS,
+            dirlinks=settings.DIRLINKS, active_page='About')
 
 def main():
-    app = Flask(__name__)
-    init_routes(app)
     app.run(**settings.SERVER_SETTINGS)
-    app.add_url_rule('/favicon.ico',
-            redirect_to=url_for('static', filename='images/favicon.ico'))
 
 if __name__ == '__main__':
     main()
