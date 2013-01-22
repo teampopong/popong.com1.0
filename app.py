281f09c527a2ad5ad68e8726171a4cd393bf1718
Cheol Kang <cornchz@gmail.com>
2013-01-22 23:46:38 +0900

    change fallback locale strategy

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ default_locale = settings.BABEL_SETTINGS['default_locale']
 def get_locale():
     locale = request.host.split('.')[0]
     if locale not in settings.LOCALES:
-        locale = settings.LOCALES[0]
+        locale = default_locale
     return locale
 
 @app.route('/')
