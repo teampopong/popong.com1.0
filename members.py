90b4282ecf94b2b8f8fe32ff569983a93ba23b61
Lucy Park <me@lucypark.kr>
2014-12-25 18:53:55 +0900

    Update member info

--- members.py
+++ members.py
@@ -4,17 +4,20 @@
 from __future__ import unicode_literals
 
 YB = {
+    "minsukkahng": ["Developer", "2014.12", ""],
     "fc_j": ["Intl. relations", "2013.10", ""],
     "strayleone": ["Developer", "2013.10", ""],
-    "ysohn": ["Politics specialist", "2013.02", ""],
     "cornchz": ["Developer", "2010.12", ""],
-    "echojuliett": ["Data miner","2011.01", ""],
     "jooddang": ["Hustler", "2013.06", ""],
-    "mincreatives": ["Public relations","2011.01","2011.12"],
-    "weirdclaire": ["UX designer","2012.08", ""]
+    "jjuntae": ["Politics specialist", "2014.06", ""],
+    "jonghyuk": ["Designer", "2014.05", ""],
+    "echojuliett": ["Data miner","2011.01", ""]
 }
 
 CONTRIBUTORS = {
+    "weirdclaire": ["UX designer","2012.08", "2014.10"],
+    "mincreatives": ["Public relations","2011.01","2011.12"],
+    "ysohn": ["Politics specialist", "2013.02", "2014.12"],
     "achimnol": ["Developer", "2010.12", ""],
     "allsolution": ["Developer","2011.07","2011.12"],
     "uni208": ["Data miner","2011.01","2011.12"],
